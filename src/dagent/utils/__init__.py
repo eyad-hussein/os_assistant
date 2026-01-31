bbff5b4ef1ccab1dd6bbc0716a6e6293bf3ba3ce
omar-abdelgawad <omar.abdelgawad@ejust.edu.eg>
2026-01-31 14:21:58 +0200

    added tests for devic.py

--- src/dagent/utils/__init__.py
+++ src/dagent/utils/__init__.py
@@ -1,4 +1,5 @@
 from .device import get_os_name
 from .logger import LOGGER
 
+# The following is not the full list because it grows really quickly
 __all__ = ["LOGGER", "get_os_name"]
--- src/dagent/utils/device.py
+++ src/dagent/utils/device.py
@@ -5,4 +5,7 @@ def get_os_name():
     os_type = platform.system()
     if os_type == "Darwin":
         return "macOS"
+    if os_type == "Linux":
+        info = platform.freedesktop_os_release()
+        os_type = f"Linux ({info.get('NAME', 'Unknown')})"
     return os_type
